6071797573a91ca889f9740589ad2d720345b01b
Alessio <alessio.luciano2005@gmail.com>
2024-03-25 16:55:17 +0100

    Added routes

--- index.js
+++ index.js
@@ -5,7 +5,35 @@ const express = require('express');
 const app = express();
 
 app.get('/', async function(req, res) {
-    res.redirect(`/home.html`);
+    
+});
+
+app.get('/sensor', async function(req, res) {
+    
+});
+
+app.get('/coordinates', async function(req, res) {
+    
+});
+
+app.get('/temperature', async function(req, res) {
+    
+});
+
+app.get('/pressure', async function(req, res) {
+    
+});
+
+app.get('/height', async function(req, res) {
+    
+});
+
+app.get('/speed', async function(req, res) {
+    
+});
+
+app.get('/camera', async function(req, res) {
+    
 });
 
 const httpServer = http.createServer(app);
@@ -14,3 +42,11 @@ var httpPort = 3001;
 httpServer.listen(httpPort, () => {
     console.log('Server HTTP in ascolto sulla porta ' + httpPort);
 });
+
+// 0 e 1 sensori
+// coord x flight controller 
+// temp
+// pressione
+// altezza 
+// velocità
+// telecamera blob
