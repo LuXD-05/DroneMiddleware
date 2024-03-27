a4dd1b0507ac903f06f7b606920fa46902d925b1
Alessio <alessio.luciano2005@gmail.com>
2024-03-27 15:05:32 +0100

    Made only 1 route for client

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const http = require('http');
 const express = require('express');
+const { log } = require('console');
 const app = express();
 
 app.get('/', async (req, res) => {
@@ -119,113 +120,30 @@ app.post('/camera', async (req, res) => {
 
 //#region GET - clientRequests
 
-app.get('/sensor', async (req, res) => {
+app.get('/client', async (req, res) => {
 
-
-    var status = null;
     var result = { };
 
     try {
-        
+        log("ok");
+
+        result.Sensor = "";
+        result.Coordinates = { x: 45.845709, y: 8.709734 };
+        result.Temperature = 0;
+        result.Pressure = 0;
+        result.Height = 0;
+        result.Speed = 0;
+        result.Image = "iVBORw0KGgoAAAANSUhEUgAAAQAAAAEACAIAAADTED8xAAADMElEQVR4nOzVwQnAIBQFQYXff81RUkQCOyDj1YOPnbXWPmeTRef+/3O/OyBjzh3CD95BfqICMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMK0CMO0TAAD//2Anhf4QtqobAAAAAElFTkSuQmCC";
+
     } catch (error) {
-        
+        log("errore");
+        res.status(500).error();
     } finally {
-        res.status(status).send(result);
-    }
-});
-
-app.get('/coordinates', async (req, res) => {
-    
-
-    var status = null;
-    var result = { };
-
-    try {
-        
-    } catch (error) {
-        
-    } finally {
-        res.status(status).send(result);
-    }
-});
-
-app.get('/temperature', async (req, res) => {
-    
-
-    var status = null;
-    var result = { };
-
-    try {
-        
-    } catch (error) {
-        
-    } finally {
-        res.status(status).send(result);
-    }
-});
-
-app.get('/pressure', async (req, res) => {
-    
-
-    var status = null;
-    var result = { };
-
-    try {
-        
-    } catch (error) {
-        
-    } finally {
-        res.status(status).send(result);
-    }
-});
-
-app.get('/height', async (req, res) => {
-    
-
-    var status = null;
-    var result = { };
-
-    try {
-        
-    } catch (error) {
-        
-    } finally {
-        res.status(status).send(result);
-    }
-});
-
-app.get('/speed', async (req, res) => {
-    
-
-    var status = null;
-    var result = { };
-
-    try {
-        
-    } catch (error) {
-        
-    } finally {
-        res.status(status).send(result);
-    }
-});
-
-app.get('/camera', async (req, res) => {
-    
-
-    var status = null;
-    var result = { };
-
-    try {
-        
-    } catch (error) {
-        
-    } finally {
-        res.status(status).send(result);
+        res.status(200).send(result);
     }
 });
 
 //#endregion
-
 
 var httpPort = 3001;
 const httpServer = http.createServer(app);
